4896115f34057d1270f04d579a5e48acd0d90b8e
BrenDev0 <lahey1991@gmail.com>
2025-12-17 16:33:53 -0600

    delete agent use_case

--- src/agents/dependencies/use_cases.py
+++ src/agents/dependencies/use_cases.py
@@ -3,6 +3,8 @@ from src.shared.domain.exceptions.dependencies import DependencyNotRegistered
 from src.shared.dependencies.container import Container
 from src.agents.application.use_cases.get_agent_by_id import GetAgentById
 from src.agents.application.use_cases.get_agents_by_user import GetAgentsByUser
+from src.agents.application.use_cases.create_agent_profile import CreateAgentProfile
+from src.agents.application.use_cases.delete_agent_profile import DeleteAgentProfile
 from src.agents.dependencies.repositories import get_agents_repository
 logger = logging.getLogger(__name__)
 
@@ -28,6 +30,36 @@ def get_agents_by_user_use_case() -> GetAgentsByUser:
     
     except DependencyNotRegistered:
         use_case  = GetAgentsByUser(
+            repository=get_agents_repository()
+        )
+        Container.register(instance_key, use_case)
+        logger.info(f"{instance_key} registered")
+    
+    return use_case
+
+
+def get_delete_agent_profile_use_case() -> DeleteAgentProfile:
+    try:
+        instance_key = "delete_agent_profile_use_case"
+        use_case = Container.resolve(instance_key)
+    
+    except DependencyNotRegistered:
+        use_case  = DeleteAgentProfile(
+            repository=get_agents_repository()
+        )
+        Container.register(instance_key, use_case)
+        logger.info(f"{instance_key} registered")
+    
+    return use_case
+
+
+def get_create_agent_profile_use_case() -> CreateAgentProfile:
+    try:
+        instance_key = "create_agent_profile_use_case"
+        use_case = Container.resolve(instance_key)
+    
+    except DependencyNotRegistered:
+        use_case  = CreateAgentProfile(
             repository=get_agents_repository()
         )
         Container.register(instance_key, use_case)
---
+++ tests/agents/unit/test_delete_agent.py
@@ -0,0 +1,112 @@
+import pytest
+from uuid import uuid4
+from unittest.mock import Mock
+from datetime import datetime
+from src.shared.domain.exceptions.repositories import NotFoundException
+from src.shared.domain.exceptions.permissions import PermissionsException
+from src.agents.application.use_cases.delete_agent_profile import DeleteAgentProfile
+from src.agents.domain.entities import Agent
+
+@pytest.fixture
+def mock_repository():
+    return Mock()
+
+@pytest.fixture
+def use_case(
+    mock_repository
+):
+    return DeleteAgentProfile(repository=mock_repository)
+
+
+def test_success(
+    mock_repository,
+    use_case: DeleteAgentProfile
+):
+    agent_id = uuid4()
+    user_id = uuid4()
+
+    fake_agent = Agent(
+        agent_id=agent_id,
+        user_id=user_id,
+        name="name",
+        description="description",
+        created_at=datetime.now()
+    )
+
+    mock_repository.get_one.return_value = fake_agent
+
+    mock_repository.delete.return_value = fake_agent
+
+    result = use_case.execute(
+        user_id=user_id,
+        agent_id=agent_id
+    )
+
+    mock_repository.get_one.assert_called_once_with(
+        key="agent_id",
+        value=agent_id
+    )
+
+    mock_repository.delete.assert_called_once_with(
+        key="agent_id",
+        value=fake_agent.agent_id
+    )
+    assert result.user_id == user_id
+    assert result.agent_id == agent_id
+
+
+def test_not_found(
+    mock_repository,
+    use_case: DeleteAgentProfile
+):
+    user_id = uuid4()
+    agent_id = uuid4()
+
+
+    mock_repository.get_one.return_value = None
+
+    with pytest.raises(NotFoundException) as exc_info:
+        result = use_case.execute(
+            user_id=user_id,
+            agent_id=agent_id
+        )
+
+    mock_repository.get_one.assert_called_once_with(
+        key="agent_id",
+        value=agent_id
+    )
+
+    mock_repository.delete.assert_not_called()
+    assert "Agent not found" in str(exc_info)
+
+
+def test_permission_error(
+    mock_repository,
+    use_case: DeleteAgentProfile
+):
+    user_id = uuid4()
+    agent_id = uuid4()
+
+    fake_agent = Agent(
+        agent_id=agent_id,
+        user_id=uuid4(),
+        name="name",
+        description="description",
+        created_at=datetime.now()
+    )
+
+    mock_repository.get_one.return_value = fake_agent
+
+    with pytest.raises(PermissionsException) as exc_info:
+        result = use_case.execute(
+            user_id=user_id,
+            agent_id=agent_id
+        )
+
+    mock_repository.get_one.assert_called_once_with(
+        key="agent_id",
+        value=agent_id
+    )
+
+    mock_repository.delete.assert_not_called()
+    assert "Forbidden" in str(exc_info)
