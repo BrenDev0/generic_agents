cb1f8f9fdf729c98727445f39c0f9395b87371b1
BrenDev0 <lahey1991@gmail.com>
2025-12-31 09:22:59 -0600

    changes for vercel deployment

--- src/app/main.py
+++ src/app/main.py
@@ -2,32 +2,32 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import logging
-import uvicorn
 from src.app.interface.fastapi.server import create_fastapi_app
 from src.app.setup.db.tables import create_tables
 
 
-def main():
-    level = os.getenv("LOGGER_LEVEL", logging.INFO)
-  
-    logging.basicConfig(
-        level=int(level),
-        format="%(levelname)s - %(name)s - %(message)s"
-    )
-    logging.getLogger("httpx").setLevel(logging.WARNING)
-    logging.getLogger("httpcore").setLevel(logging.WARNING)
-    logger = logging.getLogger(__name__)
-    logger.debug("!!!!! LOGGER LEVEL SET TO DEBUG !!!!!")
+level = os.getenv("LOGGER_LEVEL", logging.INFO)
 
-    create_tables()
+logging.basicConfig(
+    level=int(level),
+    format="%(levelname)s - %(name)s - %(message)s"
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpcore").setLevel(logging.WARNING)
+logger = logging.getLogger(__name__)
+logger.debug("!!!!! LOGGER LEVEL SET TO DEBUG !!!!!")
 
-    app = create_fastapi_app()
+create_tables()
+
+app = create_fastapi_app()
+    
+
+
+if __name__ == "__main__":
+    import uvicorn
+    
     uvicorn.run(
         app=app,
         host="0.0.0.0",
         port=8000,
     )
-
-
-if __name__ == "__main__":
-    main()
